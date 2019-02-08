6ef72d0c01c67975ced1e3b5b4e5a1d2e757e0ac
Hua Luo <hluo@lifesize.com>
2019-02-08 17:49:48 -0600

    comments out the unused thing

--- src/main/java/com/sillycat/springbootcxf/SpringBootCXFApplication.java
+++ src/main/java/com/sillycat/springbootcxf/SpringBootCXFApplication.java
@@ -1,14 +1,10 @@
 package com.sillycat.springbootcxf;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
 @SpringBootApplication
 public class SpringBootCXFApplication extends SpringBootServletInitializer {
 	
